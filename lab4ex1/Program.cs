f2e018f0c31f5ccb07ef16eff8e7d3fc20d22eaa
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-25 16:47:55 +0200

    Add project files.

---
+++ lab4ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab4ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va citi un vector de intregi de dimensiune n de la tastaura. Scrieti o
+            functie care va inversa elementele vectorului, apelati-o si afisati-I rezultatul */
+
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            for(int i = 0; i<n; i++)
+            {
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Vectorul este: " + string.Join(" ", InversVector(n, vector)));
+
+            static int[] InversVector(int n, int[] vector)
+            {
+                int[] vector2 = new int[n];
+                int j = 0;
+                for(int i = n-1; i>=0; i--)
+                {
+                    vector2[j] = vector[i];
+                    j++;
+                }
+
+                return vector2;
+            }
+
+        }
+    }
+}
